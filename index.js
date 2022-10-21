b46b0831aa6a21cf7cbc5feadcf2299ca2495f17
iamsubham-b <116329752+iamsubham-b@users.noreply.github.com>
2022-10-21 09:47:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+let a = superheroes.random();
+let b = supervillains.random();
+console.log(a);
+console.log(b);
